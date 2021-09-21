749f8afd7dc86c6193f281b786b034b537e4d900
Ryan Bender <ryan.bender.general@gmail.com>
2021-09-21 16:02:14 -0400

    home page layout update

---
+++ portfolio/PortfolioApp/static/js/home.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+    var stats = [
+        ['Age', '23'],
+        ['Years In Industry', '2'],
+        ['Main Languages', 'Java, Python, Javascript'],
+        ['Life Director', 'Jeremiah 29:11']
+    ]
+    populateStats(stats)
+})
+
+/**
+ * Populate the stats section.
+ * 
+ * @param {string[][]} stats 
+ */
+async function populateStats(stats) {
+    var statsList = $('.stats-list')
+
+    stats.forEach(stat => {
+        var title = stat[0]
+        var value = stat[1]
+
+        var item = $('<li>')
+        item.append($(`<h2>${title}:</h2>`))
+        item.append($(`<span>${value}</span>`))
+        statsList.append(item)
+    })
+}
